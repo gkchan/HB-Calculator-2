e6d188a46af03f767cfe86060db9043b68307cd6
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-24 16:27:29 -0700

    WIP - all functions working; need to debug printing the error

--- calculator.py
+++ calculator.py
@@ -12,31 +12,28 @@ from arithmetic import *
 while True:
     user_input = raw_input("> ")
     tokens = user_input.split(" ")
-    
-    if tokens[0] == 'q':
-        break
-
-    else:
-        try:
-            arg1, arg2 = float(tokens[1]), float(tokens[2])
-        except:
-            arg1 = float(tokens[1])
-        finally:
-            if tokens[0] == '+':
-                print add(arg1, arg2)
-            elif tokens[0] == '-':
-                print subtract(arg1, arg2)
-            elif tokens[0] == '*':
-                print multiply(arg1, arg2)
-            elif tokens[0] == '/':
-                print divide(arg1, arg2)
-            elif tokens[0] == "square":
-                print square(arg1)
-            elif tokens[0] == "cube":
-                print cube(arg1)
-            elif tokens[0] == "pow":
-                print power(arg1, arg2)
-            elif tokens[0] == "mod":
-                print mod(arg1, arg2)
-            else:
-                print "Not a recognized operation."
+    try:
+        arg1, arg2 = float(tokens[1]), float(tokens[2])
+    except IndexError:
+        arg1 = float(tokens[1])
+    finally:
+        if tokens[0] == 'q':
+            break
+        elif tokens[0] == '+':
+            print add(arg1, arg2)
+        elif tokens[0] == '-':
+            print subtract(arg1, arg2)
+        elif tokens[0] == '*':
+            print multiply(arg1, arg2)
+        elif tokens[0] == '/':
+            print divide(arg1, arg2)
+        elif tokens[0] == "square":
+            print square(arg1)
+        elif tokens[0] == "cube":
+            print cube(arg1)
+        elif tokens[0] == "pow":
+            print power(arg1, arg2)
+        elif tokens[0] == "mod":
+            print mod(arg1, arg2)
+        else:
+            print "Not a recognized operation."
